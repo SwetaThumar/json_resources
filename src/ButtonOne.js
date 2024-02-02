5ec0c46b6db3f33e822be9a56a2acbc4b8263a5f
SwetaThumar <thumarsweta123@gmail.com>
2024-02-02 18:47:16 +0530

    user

---
+++ src/ButtonOne.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Link } from "react-router-dom";
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+     <button><Link to="/Post">Post</Link></button>
+     <button><Link to="/Photos">Photos</Link></button>
+     <button><Link to="/Albums">Albums</Link></button>
+     <button><Link to="/Todo">Todo</Link></button> 
+     <button><Link to="/Coment">Coment</Link></button>
+     <button><Link to="/Users">Users</Link></button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,101 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ButtonOne from './ButtonOne';
+
+import React from 'react';
+
+function Users() {
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(function (response) {
+                console.log(response.data)
+                setdata(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }, [])
+    return (
+        <div className='App'>
+            <ButtonOne></ButtonOne>
+            <table border={1}>
+                <tr>
+                    <td>ID</td>
+                    <td>NAME</td>
+                    <td>USERNAME</td>
+                    <td>EMAIL</td>
+                    <td>ADDRESS</td>
+                    <td>PHONE</td>
+                    <td>WEBSITE</td>
+                    <td>COMPANY</td>
+                </tr>
+                {
+                    data.map((ele, ind) => {
+                        return (
+                            <>
+                                <tr key={ind}>
+                                    <td>{ele.id}</td>
+                                    <td>{ele.name}</td>
+                                    <td>{ele.username}</td>
+                                    <td>{ele.email}</td>
+                                    <td>
+                                        <table>
+                                            <tr >
+                                                <td>STREET</td>
+                                                <td>SUITE</td>
+                                                <td>CITY</td>
+                                                <td>ZIPCODE</td>
+                                                <td>GEO</td>
+                                            </tr>
+
+                                            <tr key={ind}>
+                                                <td>{ele.address.street}</td>
+                                                <td>{ele.address.suite}</td>
+                                                <td>{ele.address.city}</td>
+                                                <td>{ele.address.zipcode}</td>
+                                                <td>
+                                                    <table>
+                                                        <tr>
+                                                            <td>LAT</td>
+                                                            <td>LNG</td>
+                                                        </tr>
+                                                        <tr key={ind}>
+                                                            <td>{ele.address.geo.lat}</td>
+                                                            <td>{ele.address.geo.lng}</td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td>{ele.phone}</td>
+                                    <td>{ele.website}</td>
+                                    <td>
+                                        <table>
+                                            <tr>
+                                                <td>NAME</td>
+                                                <td>CATCHPHRASE</td>
+                                                <td>BS</td>
+                                            </tr>
+                                            <tr>
+                                                <td>{ele.company.name}</td>
+                                                <td>{ele.company.catchPhrase}</td>
+                                                <td>{ele.company.bs}</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </>
+                        )
+                    })
+                }
+            </table>
+        </div>
+    );
+}
+
+export default Users;
